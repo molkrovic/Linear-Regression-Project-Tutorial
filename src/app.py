312c11189e75ea8eafbe58cab7b14ed1f6e16b64
molkrovic <vicmoller18@gmail.com>
2022-07-17 13:58:06 +0000

    Décimo guardado

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
@@ -15,8 +16,7 @@ df['sex'] = df['sex'].map(sex_dict)
 smoker_dict = {'yes':1, 'no':0}
 df['smoker'] = df['smoker'].map(smoker_dict)
 
-region_dict = {'southeast':0, 'southwest':1, 'northwest':2, 'northeast':3}
-df['region'] = df['region'].map(region_dict)
+df = pd.get_dummies(df, columns = ['region'])
 
 # Dividir el dataset en variables explicativas y target
 X = df.drop(columns=['charges'])
@@ -27,11 +27,18 @@ X_train, X_test_aux, y_train, y_test_aux = train_test_split(X, y, test_size=0.3,
 X_val, X_test, y_val, y_test = train_test_split(X_test_aux, y_test_aux, test_size=0.5, random_state=11)
 
 # Construir el modelo
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_val = scaler.transform(X_val)
+X_test = scaler.transform(X_test)
+
 lin_reg = LinearRegression()
 lin_reg.fit(X_train, y_train)
 
 # Guardar el modelo
-filename = 'modelo_regresion_lineal.sav'
+filename = '../models/modelo_regresion_lineal.sav'
 pickle.dump(lin_reg, open(filename, 'wb'))
 
 # Evaluar el modelo
